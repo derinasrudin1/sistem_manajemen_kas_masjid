4e4d6a6cc32f33e7be663b5870ee33020c8b47f6
derinasrudin1 <deri.nasrudin78@gmail.com>
2025-06-30 16:49:40 +0700

    change view login

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -3,47 +3,99 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Login</title>
+    <title>Login | Nama Aplikasi Anda</title>
     <link rel="stylesheet" href="<?= base_url('adminlte/plugins/fontawesome-free/css/all.min.css') ?>">
     <link rel="stylesheet" href="<?= base_url('adminlte/dist/css/adminlte.min.css') ?>">
+
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
+    <style>
+        body.login-page {
+            background-color: #f4f6f9;
+        }
+
+        .login-box {
+            width: 540px;
+        }
+
+        .login-card-body .login-logo-in-card {
+            text-align: center;
+            margin-bottom: 2px;
+        }
+
+        .login-card-body .login-logo-in-card img {
+            width: 500px;
+            max-width: 100%;
+            height: auto;
+        }
+
+        .login-card-body .login-app-name {
+            text-align: center;
+            font-weight: bold;
+            font-size: 1.8rem;
+            margin-bottom: 20px;
+            color: #333;
+        }
+
+        .login-box-msg {
+            margin-bottom: 20px;
+        }
+
+        @media (max-width: 576px) {
+            .login-box {
+                width: 90%;
+                margin-top: 20px;
+            }
+        }
+    </style>
 </head>
 
 <body class="hold-transition login-page">
     <div class="login-box">
-        <div class="login-logo">
-            <b>Sistem</b> Login
-        </div>
-        <div class="card">
+        <div class="card card-outline card-primary">
             <div class="card-body login-card-body">
-                <p class="login-box-msg">Masuk untuk melanjutkan</p>
+
+                <div class="login-logo-in-card">
+                    <img src="<?= base_url('logo.png') ?>" alt="Logo Aplikasi">
+                </div>
+
+                <p class="login-box-msg">Login ke Sistem</p>
 
                 <?php if (session()->getFlashdata('error')): ?>
-                    <div class="alert alert-danger"><?= session()->getFlashdata('error') ?></div>
+                    <div class="alert alert-danger text-center"><?= session()->getFlashdata('error') ?></div>
                 <?php endif; ?>
 
                 <form action="<?= base_url('/auth/login') ?>" method="post">
+                    <?= csrf_field() ?>
                     <div class="input-group mb-3">
                         <input type="text" name="username" class="form-control" placeholder="Username" required>
                         <div class="input-group-append">
-                            <div class="input-group-text"><span class="fas fa-user"></span></div>
+                            <div class="input-group-text">
+                                <span class="fas fa-user"></span>
+                            </div>
                         </div>
                     </div>
+
                     <div class="input-group mb-3">
                         <input type="password" name="password" class="form-control" placeholder="Password" required>
                         <div class="input-group-append">
-                            <div class="input-group-text"><span class="fas fa-lock"></span></div>
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
                         </div>
                     </div>
+
                     <div class="row">
                         <div class="col-12">
                             <button type="submit" class="btn btn-primary btn-block">Masuk</button>
                         </div>
                     </div>
                 </form>
+
             </div>
         </div>
     </div>
-
     <script src="<?= base_url('adminlte/plugins/jquery/jquery.min.js') ?>"></script>
     <script src="<?= base_url('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
     <script src="<?= base_url('adminlte/dist/js/adminlte.min.js') ?>"></script>
